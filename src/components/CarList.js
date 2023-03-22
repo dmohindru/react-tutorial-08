85110697d6387cd8126a8907ed5e839e55c163c9
Dhruv Mohindru <dmohindru@gmail.com>
2023-03-23 06:49:34 +1100

    Finished section 20. Video 340

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -1,39 +1,48 @@
-import { useSelector, useDispatch } from "react-redux";
-import { removeCar } from "../store";
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
+
 function CarList() {
-    const dispatch = useDispatch();
-    const cars = useSelector(({cars: { data, searchTerm }}) =>{
+  const dispatch = useDispatch();
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-        return data.filter((car) => 
-            car.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-    });
-
-    const handleCarDelete = (car) => {
-        dispatch(removeCar(car.id));
+    return {
+      cars: filteredCars,
+      name: form.name,
     };
+  });
 
-    const renderedCars = cars.map((car) => {
-        return (
-            <div key={car.id} className="panel">
-                <p>
-                    {car.name} - ${car.cost}
-                </p>
-                <button 
-                    className="button is-danger"
-                    onClick={() => handleCarDelete(car)}
-                >
-                    Delete
-                </button>
-            </div>
-        )
-    })
-    
-    return (<div className="car-list">
-        {renderedCars}
-        <hr />
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
 
-    </div>);
+  const renderedCars = cars.map((car) => {
+    // DECIDE IF THIS CAR SHOULD BE BOLD
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
